e7e512b25de5342b872f9a618518c025b1a098e6
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-07-07 16:31:57 +0000

    square root done

--- app.rb
+++ app.rb
@@ -22,10 +22,10 @@ get("/square_root/new") do
   erb(:square_root_calc_new)
 end
 
-get ("/square_root/results/") do
-  @the_num = params.fetch("user_number").to_f
+get ("/square_root/results") do
+  @the_num_root = params.fetch("user").to_f
 
-  @the_result = @the_num ** 0.5
+  @the_result_root = @the_num_root ** 0.5
 
   erb(:square_root_results)
 end
