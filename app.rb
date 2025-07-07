00a795978dd272f4f6aa6a4bd29cc8bd24a0c3f2
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-07-07 18:51:25 +0000

    figure out @monthly_payment calculation

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require "sinatra/reloader"
 
+
 get("/") do
   erb(:homepage)
 end
@@ -28,4 +29,18 @@ get ("/square_root/results") do
   @the_result_root = @the_num_root ** 0.5
 
   erb(:square_root_results)
+end
+
+get("/payment/new") do
+  erb(:payment_calc_new)
+end
+
+get ("/payment/results") do
+  @user_apr = params.fetch("user_apr").to_f
+  @user_years = params.fetch("user_years").to_f
+  @user_pv = params.fetch("user_pv").to_f
+
+  @monthly_payment = pp @user_apr / 50
+
+  erb(:payment_results)
 end
