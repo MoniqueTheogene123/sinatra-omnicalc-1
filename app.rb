69dbe4d06419ade39b5c44fb3e44430f4c8d4ccd
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-07-07 15:56:32 +0000

    commit

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ get("/") do
 end
 
 get("/square/new") do
-  erb(:square_root_new)
+  erb(:square_calc_new)
 end
 
 get ("/square/results") do
@@ -15,4 +15,17 @@ get ("/square/results") do
   @the_result = @the_num ** 2
 
   erb(:square_results)
+end
+
+
+get("/square_root/new") do
+  erb(:square_root_calc_new)
+end
+
+get ("/square_root/results/") do
+  @the_num = params.fetch("user_number").to_f
+
+  @the_result = @the_num ** 0.5
+
+  erb(:square_root_results)
 end
